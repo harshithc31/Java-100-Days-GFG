289eee5174a83fb86e4d26ed2857c2bb020b00ac
harshithc31 <132904402+harshithc31@users.noreply.github.com>
2023-12-17 19:33:55 +0530

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,13 @@
+# Missing number in array
+class Solution {
+    int missingNumber(int array[], int n) {
+        // Your Code Here
+        int total = (n*(n+1))/2;
+        int sum = 0;
+        
+        for(int i=0; i<n-1; i++) {
+            sum += array[i];
+        }
+        return total-sum;
+    }
+}
