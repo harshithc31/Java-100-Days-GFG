71ab303f380a5046836cab1483859b33e800d05f
harshithc31 <132904402+harshithc31@users.noreply.github.com>
2023-12-24 23:34:51 +0530

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,20 @@
+// Max Sum without Adjacents
+class Solution {
+    int findMaxSum(int arr[], int n) {
+        // code here
+    
+        if(n == 1) {
+            return arr[0];
+        }
+        
+        int num1 = arr[0];
+        int num2 = Math.max(arr[1], arr[0]);
+        
+        for(int i = 2; i < n; i++) {
+            int temp = num2;
+            num2 = Math.max(num1 + arr[i], num2);
+            num1 = temp;
+        }
+        return num2;
+    }
+}
