8237c94005a6f93eba9fcf47c7a300cae6f02bc4
harshithc31 <132904402+harshithc31@users.noreply.github.com>
2024-01-27 17:25:53 +0530

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,46 @@
+//Determinant of a Matrix
+//Given a square matrix of size n*n. The task is to find the determinant of this matrix.
+
+class Solution
+{
+    //Function for finding determinant of matrix.
+    static int determinantOfMatrix(int matrix[][], int n)
+    {
+        // code here 
+        int det=0;
+        
+        if( n ==1 ){
+            return matrix[0][0];
+        }
+        else if( n==2 ){
+            return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+        }
+        
+        for(int i=0;i<n;i++){
+            int[][] b = new int[n-1][n-1];
+            int l=0,m=0;
+            for(int j=1;j<n;j++){
+                m=0;
+                for(int k = 0;k<n;k++){
+                    if( k == i){
+                        continue;
+                    }
+                    b[l][m] = matrix[j][k];
+                    m++;
+                }
+                l++;
+            }
+            
+            int coFactor = matrix[0][i]*determinantOfMatrix(b,n-1);
+            if(i%2==0){
+                det += coFactor;
+            }
+            else{
+                det -=coFactor;
+            }
+            
+        }
+        
+        return det;
+    }
+}
